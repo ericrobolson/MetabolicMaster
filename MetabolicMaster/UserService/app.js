145e376512054aacf3f572e063dbe1769f79d0a1
Eric Olson <eric.rob.olson@gmail.com>
2018-02-24 09:15:25 -0800

    #7: Started implementing PUT for User Service.

--- MetabolicMaster/UserService/app.js
+++ MetabolicMaster/UserService/app.js
@@ -2,6 +2,7 @@
 
 // User service
 
+var sqlite3 = require('sqlite3').verbose();
 var express = require('express');
 var app = express();
 
@@ -14,6 +15,41 @@ app.get('/:email', function (req, res) {
 });
 
 app.post('/:email', function(req, res){
+	var hasError = false;
+	var email = req.params.email;
+	
+	console.log('requested email: ' + email);
+	
+	var db = new sqlite3.Database('db/user_database.db', (err) => {
+		if (err){
+			hasError = true;
+		}
+	});
+		
+	let sql = 'SELECT Id FROM USER WHERE Email = ?;';
+	
+	var userId = -1;
+	
+	db.get(sql, [email], (err, row) =>{
+		if (err){
+			hasError = true;
+		}
+		else{
+			userId = row.id;			
+		}		
+	});
+	
+	db.close((err) => {
+		if (err){
+			hasError = true;
+		}
+	});
+	
+	if (hasError === true){
+		res.status(500).send('internal server error.');
+		return;
+	};
+	
 	res.status(501).send('not implemented');
 });
 
